f274bb305020596ad473b92ac8a33dd32765c760
Luis Santos <luis.santos.taiba@gmail.com>
2023-02-02 21:33:07 -0300

    feat: prop types

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Header from './Header';
 function App() {
   return (
     <>
-      <Header title="JStack's Blog">
+      <Header>
         <h2>Posts da semana</h2>
       </Header>
 
@@ -17,6 +17,23 @@ function App() {
           title: 'Título da notícia 01',
           subtitle: 'Subtítulo da notícia 01',
         }}
+        likes={20}
+      />
+
+      <Post
+        post={{
+          title: 'Título da notícia 02',
+          subtitle: 'Subtítulo da notícia 02',
+        }}
+        likes={10}
+      />
+
+      <Post
+        post={{
+          title: 'Título da notícia 03',
+          subtitle: 'Subtítulo da notícia 03',
+        }}
+        likes={50}
       />
     </>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Header(props) {
+export default function Header(props) {
   return (
     <>
       <h1>{props.title}</h1>
@@ -9,4 +10,11 @@ function Header(props) {
   );
 }
 
-export default Header;
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+Header.defaultProps = {
+  title: `JStack's Blog`,
+};
