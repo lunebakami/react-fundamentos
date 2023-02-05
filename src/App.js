dac20bdf5922362f4c13383ef129bd535dad2335
Luis Santos <luis.santos.taiba@gmail.com>
2023-02-04 21:39:18 -0300

    feat: use state hook

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Post from './Post';
 import Header from './Header';
 
-const posts = [
-  {
-    title: 'Title#01',
-    subtitle: 'Sub#01',
-    likes: 20,
-  },
-  {
-    title: 'Title#02',
-    subtitle: 'Sub#02',
-    likes: 10,
-  },
-  {
-    title: 'Title#03',
-    subtitle: 'Sub#03',
-    likes: 50,
-  },
-];
-
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: Math.random(),
+      title: 'Title#01',
+      subtitle: 'Sub#01',
+      likes: 20,
+    },
+    {
+      id: Math.random(),
+      title: 'Title#02',
+      subtitle: 'Sub#02',
+      likes: 10,
+    },
+    {
+      id: Math.random(),
+      title: 'Title#03',
+      subtitle: 'Sub#03',
+      likes: 50,
+    },
+    {
+      id: Math.random(),
+      title: 'Title#04',
+      subtitle: 'Sub#04',
+      likes: 50,
+    },
+  ]);
+
+  function handleRefresh() {
+    setTimeout(() => {
+      setPosts((prevState) => [
+        ...prevState,
+        {
+          id: Math.random(),
+          title: `Title#0${prevState.length + 1}`,
+          subtitle: `Sub#0${prevState.length + 1}`,
+          likes: 50,
+        },
+      ]);
+    }, 2000);
+  }
+
   return (
     <>
       <Header>
-        <h2>Posts da semana</h2>
+        <h2>
+          Posts da semana
+          <button onClick={handleRefresh}>Atualizar</button>
+        </h2>
       </Header>
 
       <hr />
