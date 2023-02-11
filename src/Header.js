0418890745de73a8c7187061d0c28dce56f892f3
Luis Santos <luis.santos.taiba@gmail.com>
2023-02-10 22:47:58 -0300

    feat: inline styles

--- src/Header.js
+++ src/Header.js
@@ -8,11 +8,16 @@ export default function Header(props) {
   const { onToggleTheme } = useContext(ThemeContext);
 
   return (
-    <>
+    <div
+      style={{
+        background: '#ccc',
+        fontFamily: 'sans-serif',
+      }}
+    >
       <h1>{props.title}</h1>
       <Button onClick={onToggleTheme}>Mudar tema</Button>
       {props.children}
-    </>
+    </div>
   );
 }
 
