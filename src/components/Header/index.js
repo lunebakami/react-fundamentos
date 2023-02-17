06c8badfe44c267e223c2e8d7928c6f193f111f3
Luis Santos <luis.santos.taiba@gmail.com>
2023-02-16 21:34:44 -0300

    styles: change header and post to styled components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,21 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
 import Button from '../Button';
 
 import { ThemeContext } from '../../context/ThemeContext';
 
-import styles from './Header.scss';
+const Title = styled.h1`
+  color: #637bf3;
+`;
 
 export default function Header(props) {
   const { onToggleTheme } = useContext(ThemeContext);
 
   return (
     <div>
-      <h1 className={styles.title}>{props.title}</h1>
+      <Title>{props.title}</Title>
       <Button onClick={onToggleTheme}>Mudar tema</Button>
       {props.children}
     </div>
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import PostHeader from './PostHeader';
 
+import PostHeader from './PostHeader';
 import styles from './Post.scss';
+
+import { Subtitle, Rate } from './styles';
 
 export default function Post(props) {
   return (
@@ -15,10 +17,9 @@ export default function Post(props) {
           read: props.post.read,
         }}
       />
-      <br />
-      <small>{props.post.subtitle}</small>
-      <br />
-      Likes: {props.post.likes / 2}
+
+      <Subtitle>{props.post.subtitle}</Subtitle>
+      <Rate>Media: {props.post.likes / 2}</Rate>
     </article>
   );
 }
