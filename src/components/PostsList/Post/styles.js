98346d64edf167e9a19be04f3b5d943fcf60e8ee
Luis Santos <luis.santos.taiba@gmail.com>
2023-02-21 08:09:10 -0300

    styles: get styles from theme

--- src/components/PostsList/Post/styles.js
+++ src/components/PostsList/Post/styles.js
@@ -4,8 +4,8 @@ export const Container = styled.article`
   background: ${({ theme }) => theme.postBackgroundColor};
   display: flex;
   flex-direction: column;
-  padding: 16px;
-  border-radius: 10px;
+  padding: ${({ theme }) => theme.spacing.medium}px;
+  border-radius: ${({ theme }) => theme.borderRadius};
 
   h2 {
     margin: 0 0 8px;
