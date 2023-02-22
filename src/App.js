52aee4bbb6ade4d3356d8754b740f3a1c054596d
Luis Santos <luis.santos.taiba@gmail.com>
2023-02-22 11:05:08 -0300

    feat: store theme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from './styles/global';
@@ -7,7 +7,9 @@ import Layout from './components/Layout';
 import themes from './styles/themes';
 
 function App() {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState(
+    JSON.parse(localStorage.getItem('theme')) || 'dark'
+  );
 
   const currentTheme = useMemo(() => {
     return themes[theme] || themes.dark;
@@ -16,6 +18,10 @@ function App() {
   function handleToggleTheme() {
     setTheme((prevState) => (prevState === 'dark' ? 'light' : 'dark'));
   }
+
+  useEffect(() => {
+    localStorage.setItem('theme', JSON.stringify(theme));
+  }, [theme]);
 
   return (
     <ThemeProvider theme={currentTheme}>
