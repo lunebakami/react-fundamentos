4138559353ac2022dca811b8e29cfafba90962aa
Luis Santos <luis.santos.taiba@gmail.com>
2023-03-07 21:41:38 -0300

    fix: remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect } from 'react';
+import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from './styles/global';
