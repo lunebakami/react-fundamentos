b7f465ff2332969fa7a579df9e79be468b0a2add
Luis Santos <luis.santos.taiba@gmail.com>
2023-03-22 22:43:50 -0300

    feat: params in class components

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -1,15 +1,34 @@
-import React, { useMemo } from 'react';
-import { useParams, useLocation } from 'react-router-dom';
+import React, { Component } from 'react';
 
-export default function Post() {
-    const params = useParams();
-    const { search } = useLocation();
-    const queryParams = useMemo(() => new URLSearchParams(search), [search]);
+export default class Post extends Component {
+    constructor(props) {
+        super(props);
 
-    console.log(params);
-    console.log(queryParams.get('name'));
+        const { search } = this.props.location;
+        this.queryParams = new URLSearchParams(search);
+    }
 
-    return (
-        <h1>Post Page</h1>
-    )
- }
+    render() {
+        console.log(this.queryParams.get('meuQueryParam'));
+
+        return (
+          <h1>
+              Post Page
+          </h1>
+        );
+    }
+}
+
+// export default function Post() {
+//     const params = useParams();
+//     const { search } = useLocation();
+//     const queryParams = useMemo(() => new URLSearchParams(search), [search]);
+//
+//     console.log(params);
+//     console.log(queryParams.get('name'));
+//
+//     return (
+//         <h1>Post Page</h1>
+//     )
+//  }
+
